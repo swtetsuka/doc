c7f0f2f5c696b06d0b7708c5676b8a2f691b212c
Tadashi Tetsuka <tetsuka@serverworks.co.jp>
2019-12-14 21:46:21 +0900

    Add Docstring

--- 2019121201/src/kana2romaji.py
+++ 2019121201/src/kana2romaji.py
@@ -1,14 +1,32 @@
 import pykakasi
 
-# 漢字仮名混じり文をローマ字に変換するスクリプト
-
 def convert_to_romaji(text):
+    """
+    漢字仮名混じり文をローマ字に変換する
+
+    Parameters
+    ----------
+    text : str
+        ローマ字に変換したい漢字仮名交じり文
+
+    Returns
+    -------
+    result : str
+        ローマ字に変換した文字列
+
+    Notes
+    -------
+    下記の Example を参考に作成しました
+    https://github.com/miurahr/pykakasi  
+    """
+
     kakasi = pykakasi.kakasi()
-    kakasi.setMode("H", "a")  # Hiragana to ascii, default: no conversion
-    kakasi.setMode("K", "a")  # Katakana to ascii, default: no conversion
-    kakasi.setMode("J", "a")  # Japanese to ascii, default: no conversion
-    kakasi.setMode("r", "Hepburn")  # default: use Hepburn Roman table
-    kakasi.setMode("s", True)  # add space, default: no separator
+    kakasi.setMode("H", "a")        # Hiragana to ascii
+    kakasi.setMode("K", "a")        # Katakana to ascii
+    kakasi.setMode("J", "a")        # Japanese to ascii
+    kakasi.setMode("r", "Hepburn")  # use Hepburn Roman table
+    kakasi.setMode("s", True)       # add space
+    kakasi.setMode("C", False)      # no capitalize
     conv = kakasi.getConverter()
     result = conv.do(text)
     return result
@@ -17,4 +35,3 @@ if __name__ == "__main__":
     text = input()
     romaji = convert_to_romaji(text)
     print(romaji)
-
